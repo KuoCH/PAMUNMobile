36aeb2d5575cf3c951e40c6e8856f21f33c7897e
KuoCH <Kuo@Mac>
2014-06-19 16:44:40 +0800

    md5 for app.zip. Small script for update zip file, md5 file and zipfile and md5 constant in ios project

---
+++ update_zip_md5.sh
@@ -0,0 +1,6 @@
+rm -rf app.zip
+zip -r app.zip app
+cp app.zip ~/Development/iOS\ WorkSpace/panasiamodelunitednations/
+MD5=$(md5 -q app.zip)
+echo $MD5 > app.zip.md5
+sed -i "" "s/^\(#define APP_AIP_MAIN_BUNDLE_MD5 \).*$/\1@\"$MD5\"/g" ~/Development/iOS\ WorkSpace/panasiamodelunitednations/panasiamodelunitednations/PamunDownloadHelper.h
