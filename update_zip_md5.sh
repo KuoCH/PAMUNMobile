767762df2a1059242a23c9f75fc6159f4247208c
KuoCH <Kuo@Mac>
2014-08-20 10:06:01 +0800

    when running the script update_zip_md5.sh, upload files to ftp

--- update_zip_md5.sh
+++ update_zip_md5.sh
@@ -5,3 +5,4 @@ cp app.zip ~/Development/iOS\ WorkSpace/panasiamodelunitednations/
 MD5=$(md5 -q app.zip)
 echo $MD5 > app.zip.md5
 sed -i "" "s/^\(#define APP_AIP_MAIN_BUNDLE_MD5 \).*$/\1@\"$MD5\"/g" ~/Development/iOS\ WorkSpace/panasiamodelunitednations/panasiamodelunitednations/PamunDownloadHelper.h
+ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app.zip app.zip.md5 css fonts icons index.html js
