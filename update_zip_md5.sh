5988a0df9798e9a8a51e5db2d7405cf0ec6a05e4
KuoCH <Kuo@Mac>
2014-08-25 03:15:05 +0800

    Add argument --upload-all and --upload-app to update_zip_md5.sh

--- update_zip_md5.sh
+++ update_zip_md5.sh
@@ -5,4 +5,9 @@ cp app.zip ~/Development/iOS\ WorkSpace/panasiamodelunitednations/
 MD5=$(md5 -q app.zip)
 echo $MD5 > app.zip.md5
 sed -i "" "s/^\(#define APP_AIP_MAIN_BUNDLE_MD5 \).*$/\1@\"$MD5\"/g" ~/Development/iOS\ WorkSpace/panasiamodelunitednations/panasiamodelunitednations/PamunDownloadHelper.h
-ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app.zip app.zip.md5 css fonts icons index.html js
+if [ "$1" == "--upload-app" ]; then
+  ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app.zip app.zip.md5 
+elif [ "$1" == "--upload-all" ]; then
+  ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app.zip app.zip.md5 
+  ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile css fonts icons index.html js
+fi
