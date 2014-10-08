d5dc8a8c17dbfda07f5c01d9632129be2ab0d634
KuoCH <Kuo@Mac>
2014-10-08 15:44:01 +0800

    git ignore app, app.zip and app.zip.m5

--- app/js/checkmobile.js
+++
@@ -1,4 +0,0 @@
-// Jump to desktop version of website if client is not mobile device
-if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  window.location="http://www.panasiamun.com/";
-}
--- app/js/page.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  new OnPress('#goback');
-  $('#goback').on('tap', function(e) {
-    e.preventDefault();
-    window.history.back();
-    return false;
-  });
-
-});
