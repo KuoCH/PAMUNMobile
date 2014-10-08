659ab0123da0b4d4ff1273322dae67c777367dab
KuoCH <Kuo@Mac>
2014-10-08 15:27:41 +0800

    update android workspace package

--- update_zip_md5.sh
+++ update_zip_md5.sh
@@ -3,6 +3,7 @@ cp -r website app
 rm -rf app.zip
 zip -9 -r app.zip app
 cp app.zip ~/Development/iOS\ WorkSpace/panasiamodelunitednations/
+cp app.zip ~/Development/Android\ Workspace/PanAsiaMUN/assets/
 MD5=$(md5 -q app.zip)
 echo $MD5 > app.zip.md5
 sed -i "" "s/^\(#define APP_AIP_MAIN_BUNDLE_MD5 \).*$/\1@\"$MD5\"/g" ~/Development/iOS\ WorkSpace/panasiamodelunitednations/panasiamodelunitednations/PamunDownloadHelper.h
