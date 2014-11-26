9aa369159a9c1c2546107bed1f0a0e4ad99fbddf
KuoCH <Kuo@Mac>
2014-11-27 06:03:51 +0800

    update page Position Paper and recompress photo of team member

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 find . -name ".DS_Store" -exec rm -f {} \;
 ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app.zip app.zip.md5 
 if [ "$1" == "--all" ]; then
-  ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app/*
+  ncftpput -R -v -u panasiamuncom -p P3N940R4 panasiamun.com /httpdocs/mobile app/* assets
 fi
